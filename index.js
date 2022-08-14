e2eba22cdb1bed3ca1c49830baff31ddccbf3065
pavelsjo <pavels.jo@gmail.com>
2022-08-14 11:17:14 -0300

    Shadows, blends and linewidths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelectorAll('canvas');
 
-const drawRectang = ({context, w = 100, h = 100, degrees = -45, fill = 'blue', stroke = 'black'}) => {
+const drawRectang = ({context, w = 100, h = 100, degrees = -45, fill = 'blue', stroke = 'black', blend='source-over'}) => {
     
     const radius =  w;
     const angle  = degreesToRadians(degrees);
@@ -12,17 +12,29 @@ const drawRectang = ({context, w = 100, h = 100, degrees = -45, fill = 'blue', s
     context.translate(rx * -0.5, (ry + h) * -0.5);               
     context.strokeStyle = stroke;
     context.fillStyle = fill;
-    context.lineWidth = 5;
+    context.lineWidth = 10;
+    context.globalCompositeOperation = blend;
+    context.shadowColor = `rgba(0, 0, 0, 0.5)`;
+    context.shadowOffsetX = -5;
+    context.shadowOffsetY =  5;    
 
     context.beginPath();
     context.moveTo(0, 0);
     context.lineTo(rx, ry);
     context.lineTo(rx, ry + h);
     context.lineTo(0, h);
-    context.closePath();    
-    context.stroke();
+    context.closePath();
     context.fill();
 
+    context.shadowColor = null;
+    context.stroke();
+
+    context.globalCompositeOperation = 'source-over';
+
+    context.lineWidth = 2;
+    context.strokeStyle = 'black';
+    context.stroke();
+    
     context.restore();
 
 };
@@ -35,30 +47,31 @@ const sketch = (color) => {
         context.fillRect(0, 0, width, height);
 
         // draw
-        const cant_rects = 50;
+        const cant_rects = 40;
         const rects = [];
         
         for(let i = 0; i < cant_rects; i++) {
     
             // params
-            const x = random(width);
-            const y = random(height);
-            const w = random(600);
-            const h = random(200);
+            const x = random(0, width);
+            const y = random(0, height);
+            const w = random(600, width);
+            const h = random(40, 200);
             const degrees = -30;
-
+            
             const fill = randomRGBA(color);
             const stroke = randomRGBA(color);
+            const blend = (Math.random() > 0.5) ? 'overlay' : 'source-over';
             rects.push({x, y, w, h, degrees, fill, stroke});
         };
         rects.forEach(rect => {
 
-            const {x, y, w, h, degrees, fill, stroke} = rect;
+            const {x, y, w, h, degrees, fill, stroke, blend} = rect;
 
             // draw block
             context.save();
             context.translate(x, y);
-            drawRectang({context, w, h, degrees, fill, stroke});
+            drawRectang({context, w, h, degrees, fill, stroke, blend});
             context.restore();
         });
     };
